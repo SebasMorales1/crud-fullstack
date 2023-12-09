eb2ffa0da5bdf47a234a9ce7e88123cb77b5c9d8
SebastianXXI05 <kaynandredblue@gmail.com>
2023-12-09 10:08:22 -0500

    Init

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import morgan from 'morgan'
+import tasksRoute from './routes/tasks.routes.js'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(tasksRoute)
+
+export default app
